cae582976bb1b5717dbcef810d8bd575fb7a3c60
Dav2257 <thepantoms586@gmail.com>
2025-05-22 13:36:27 +0700

    index

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,17 @@
 interface Env {
-    ASSETS: Fetcher;
-    DB: D1Database;
-  }
-  
-  export default {
-    async fetch(request, env) {
-      const url = new URL(request.url);
-  
-      if (url.pathname.startsWith("/api/nomnoms")) {
-        let { results } = await env.DB.prepare("SELECT * FROM nomnoms").all();
-        return Response.json(results);
-      }
-  
-      return env.ASSETS.fetch(request);
-    },
-  }
+  ASSETS: Fetcher;
+  DB: D1Database;
+}
+
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    const url = new URL(request.url);
+
+    if (url.pathname.startsWith("/api/nomnoms")) {
+      const { results } = await env.DB.prepare("SELECT * FROM nomnoms").all();
+      return Response.json(results);
+    }
+
+    return env.ASSETS.fetch(request);
+  },
+};
